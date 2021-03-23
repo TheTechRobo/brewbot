6556b519af42345f79dee8891683fdcf57d167db
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-03-23 19:43:14 -0400

    improvements

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 """
-This is the rewrite that is in progress for the Brew Discord bot. It uses the bot.command rather than the client.event since I think that it is better maybe??? I really dont know.
+This is the rewrite that is in progress for the Brew Discord bot.
+It uses the bot.command rather than the client.event since it's much
+easier to maintain.
 """
-import discord, configparser
+import configparser
 from discord.ext import commands
 from discord.ext.commands import Bot
 
 bot = Bot(command_prefix='brew ') #makes the prefix << brew >>
-
-
 
 @bot.command(name='test')
 async def test(context): #needs the context - Context means that it will send it in the channel that the message was sent in.
@@ -16,25 +16,35 @@ async def test(context): #needs the context - Context means that it will send it
 
 @bot.command(name='spam')
 async def spam(context, END): #context is the content, end is the last thing
+    """
+    Takes one parameter: how mahy times to spam.
+    """
     channel = context.channel
     print(channel.name)
     if channel.name == 'brew-spamming':
         pass
     else:
         print(channel.name == "brew-spamming") #logging
-        await context.send(f'Please only use this command in the correct channel') #cant figure this out. it does equal brew-spamming and it still does this...g
+        await context.send('Please only use this command in the correct channel')
         return
     for i in range(0, int(END)):
-        await context.send(f'Brew!! :beer:')
+        await context.send('Brew!! :beer:')
+        del i
 
 @commands.cooldown(1, 30, commands.BucketType.user)
 @bot.command(name='mine')
-async def test(context): #needs the context - Context means that it will send it in the channel that the message was sent in.
-    user=context.author
+async def mine(context):
+    """
+    Nobody except TRM knows why the HECK this is here.
+    """
+    user = context.author
     await context.send(f'Hi {user}, this is testing')
 
 @bot.event
 async def on_command_error(ctx, error):
+    """
+    Does some stuff.
+    """
     if isinstance(error, commands.CommandOnCooldown):
         await ctx.send(f'The drill is overheated. You cannot brewcoin yet. Please wait {int(error.retry_after)}s.')
     raise error
