ebd9e2ae3165134dc4ec95c042d4f3c92923eafb
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-03-22 22:07:42 -0400

    BREW

--- newo.py
+++ newo.py
@@ -14,8 +14,10 @@ async def on_message(message):
     if message.content == "use for future":
         try:
             brewscores.scores[message.author] += 1
+            return
         except:
             brewscores.scores[message.author] = 1
+            return
     if message.content.startswith('brew'):
         if message.content.startswith('brew bal'):
             await message.channel.send('BALANCE: 0 because this bot isnt finished!!!!!!!111111')
@@ -27,7 +29,6 @@ async def on_message(message):
             for i in range(0, numberOfTimes): #sends it 9 times
                 await message.channel.send('brew :beer:')
             return
-        elif message.content.startswith('brew'):
-            await message.channel.send('Brew!! :beer: :beer:')
+        await message.channel.send('Brew!! :beer: :beer:')
 
 client.run('ODIzNzIyNDk5MDU3Mzg1NDkz.YFk9Ww.7np2a793tTK4H061CXbu2O_Yh20')
