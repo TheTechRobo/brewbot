517d3e3fb2219afc755d811f5d7a76ef45db8b2f
TRM <theruntingmuumuu@gmail.com>
2021-03-22 21:25:22 -0400

    spam

--- newo.py
+++ newo.py
@@ -15,6 +15,18 @@ async def on_message(message):
         if message.content.startswith('brew bal'):
             await message.channel.send('BALANCE: 0 because this bot isnt finished!!!!!!!111111')
             return
+        elif message.content.startswith('brew spam'):
+            await message.channel.send('brew :beer:')
+            await message.channel.send('brew :beer:')
+            await message.channel.send('brew :beer:')
+            await message.channel.send('brew :beer:')
+            await message.channel.send('brew :beer:')
+            await message.channel.send('brew :beer:')
+            await message.channel.send('brew :beer:')
+            await message.channel.send('brew :beer:')
+            await message.channel.send('brew :beer:')
+            await message.channel.send('brew :beer:')
+            await message.channel.send('brew :beer:')
         await message.channel.send('Brew!! :beer: :beer:')
 
 client.run('ODIzNzIyNDk5MDU3Mzg1NDkz.YFk9Ww.7np2a793tTK4H061CXbu2O_Yh20')
