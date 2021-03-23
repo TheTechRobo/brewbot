051a06262a1f7e96a31a08dcada6a0243cd9cd22
TRM <theruntingmuumuu@gmail.com>
2021-03-23 19:35:18 -0400

    a

--- index.py
+++ index.py
@@ -7,23 +7,36 @@ from discord.ext.commands import Bot
 
 bot = Bot(command_prefix='brew ') #makes the prefix << brew >>
 
+
+
 @bot.command(name='test')
 async def test(context): #needs the context - Context means that it will send it in the channel that the message was sent in.
     user=context.author
     await context.send(f'Hi {user}, you are senche raht :beer:')
 
 @bot.command(name='spam')
-async def spam(context, END): #needs the context - Context means that it will send it in the channel that the message was sent in.
-    await context.send(f'Magden')
+async def spam(context, END): #context is the content, end is the last thing
     channel = context.channel
     print(channel.name)
     if channel.name == 'brew-spamming':
         pass
-    else: 
-        print(channel.name == "brew-spamming")
+    else:
+        print(channel.name == "brew-spamming") #logging
         await context.send(f'Please only use this command in the correct channel') #cant figure this out. it does equal brew-spamming and it still does this...g
         return
     for i in range(0, int(END)):
         await context.send(f'Brew!! :beer:')
+
+@commands.cooldown(1, 30, commands.BucketType.user)
+@bot.command(name='mine')
+async def test(context): #needs the context - Context means that it will send it in the channel that the message was sent in.
+    user=context.author
+    await context.send(f'Hi {user}, this is testing')
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send(f'The drill is overheated. You cannot brewcoin yet. Please wait {int(error.retry_after)}s.')
+    raise error
 
 bot.run('ODIzNzIyNDk5MDU3Mzg1NDkz.YFk9Ww.7np2a793tTK4H061CXbu2O_Yh20')
