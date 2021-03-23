7574fa8a32091c9a1a22c5fe2ceee554b3f9719c
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-03-22 22:03:58 -0400

    limit channels used in spam

--- newo.py
+++ newo.py
@@ -8,6 +8,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    print(message.channel)
     if message.author == client.user:
         return
     if message.content == "use for future":
@@ -20,7 +21,7 @@ async def on_message(message):
             await message.channel.send('BALANCE: 0 because this bot isnt finished!!!!!!!111111')
             return
         elif message.content.startswith('brew spam'):
-            print(message)
+            if message.channel.name != "brew-spamming": return
             numberOfTimes = int(str(message.content).split()[2])
             print(numberOfTimes)
             for i in range(0, numberOfTimes): #sends it 9 times
