351f036a7c83591963c94f4b08d91f0ae7e0da57
TRM <theruntingmuumuu@gmail.com>
2021-03-23 09:14:10 -0400

    better error checking, better logging, stops too much spamming

--- newo.py
+++ newo.py
@@ -1,3 +1,8 @@
+"""
+BrewBot
+NOTE TO TRM : REMEMBER TO USE RETURN TO END THINGS
+"""
+
 import discord, configparser
 
 scores = configparser.ConfigParser()
@@ -9,7 +14,8 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    print(message.channel)
+    print("\n\nMessage sent in ", message.channel)
+    print(message.author, "sent the message <<", message.content, ">>")
     if message.author == client.user:
         return
     if message.content == "brew":
@@ -30,12 +36,20 @@ async def on_message(message):
         elif message.content.startswith('brew spam'):
             if message.channel.name != "brew-spamming":
                 await message.channel.send('Please only spam brews in the #brew-spamming channel :beer:')
-                return
-            numberOfTimes = int(str(message.content).split()[2])
-            print(numberOfTimes)
-            for i in range(0, numberOfTimes): #sends it 9 times
+                return #exits the elif statement, does not execute the brew spam
+            try:
+                numberOfTimes = int(str(message.content).split()[2])
+                print(numberOfTimes)
+                if numberOfTimes <= 15: #limits spamming to 15
+                    for i in range(0, numberOfTimes): #sends it 9 times
+                        await message.channel.send('brew :beer:')
+                    return
+                else:
+                    await message.channel.send("I don't want to block this, but it will probably really lag the server... So please limit auto spamming brew to 15...")
+                    return
+            except:
                 await message.channel.send('brew :beer:')
-            return
+                return
         await message.channel.send('Brew!! :beer: :beer:')
 
 client.run('ODIzNzIyNDk5MDU3Mzg1NDkz.YFk9Ww.7np2a793tTK4H061CXbu2O_Yh20')
