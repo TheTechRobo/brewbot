0d8f116a711e414800eacaadccd6f865b104837e
TRM <theruntingmuumuu@gmail.com>
2021-03-25 11:02:04 -0400

    a

--- store_data.py
+++ store_data.py
@@ -1,2 +1,2 @@
 STORE_DATA = {"Brew": 30, "Giant Brew": 50}
-hi = '''brewbot is unfortunately closed-source at the request of the rat named " TRM "! It goes against all of thetechrobo's values but TRM doesn't care!'''
+hi = '''Brewbot is unfortunately currently closed source. '''
