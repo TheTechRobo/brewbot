f1975de3e9889583dfd49b4ec9b613ff8e518d22
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-03-25 21:15:24 -0400

    credits

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 """
+https://stackoverflow.com/a/59555898/9654083
 https://stackoverflow.com/a/65436253/9654083
 https://stackoverflow.com/questions/60055037/how-make-custom-status-discord-py
 This is the rewrite that is in progress for the Brew Discord bot.
