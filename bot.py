e9c2600e0959a2ba640ac92df72563276c136a07
TRM <theruntingmuumuu@gmail.com>
2021-03-29 19:48:25 -0400

    a

--- bot.py
+++ bot.py
@@ -24,21 +24,30 @@ initial_extensions = ['fun2', 'system', 'brewcoin2']
 
 for extension in initial_extensions: #runs the amount of times of files to load
     bot.load_extension(extension) #loads
-    print(extension)
-    print("Magplar (loaded the extensions)")
+    print(f'\nThe {extension} has loaded')
 
 async def status(): #function for changing the status
-    print("Stamden (changing the status)") #console.log
+    print("\nA stamden has changed the bot status") #console.log
     choices = ["a river","brew out of the faucet"] #what is can be changed to
     await bot.change_presence(activity=discord.Streaming(url="https://www.youtube.com/watch?v=ivSOrKAsPss", name=random.choice(choices))) #changes it, link required for streaming status to work
 
 #--When the bot loads--
 @bot.event
 async def on_ready():
-    print("Hits-Head-on-Cabinet has logged in...") #tell console bot is logged in
+    print("\n-----------------------------------------------\n<----Hits-Head-on-Cabinet has logged in...---->\n-----------------------------------------------") #tell console bot is logged in
     while True: #repeat forever
         await status()
         await asyncio.sleep(20)
+
+@bot.event
+async def on_command_error(ctx, error):
+    """
+    Does some stuff in case of cooldown error.
+    """
+    if isinstance(error, commands.CommandOnCooldown):
+        potentialMessages = [f'This command is on cooldown, please wait {int(error.retry_after)}s.', f'Searching for more coins to excavate... ({int(error.retry_after)}s)', f'The GPU overheated. Hopefully it did not die, or you may have a hard time finding a new one... {int(error.retry_after)}s.', f'You should not be greedy and mine too many brewcoins... Please try again in {int(error.retry_after)}s.', f'The drill is overheated. You cannot brewcoin yet. Please wait {int(error.retry_after)}s.', f'Bad things may happen if you do not wait {int(error.retry_after)} more seconds before mining again... :ghost:']
+        await ctx.send(random.choice(potentialMessages))
+        print('\nAn anonymous magcro tried to do a command that was on cooldown')
 
 def TheColoursOfTheRainbow(): #to choose a random RGB value
     colours = []
--- brewcoin2.py
+++ brewcoin2.py
@@ -1,4 +1,3 @@
-print(5)
 import discord
 from discord.ext import commands
 import configparser
