c32149a38486c404128512c6b420bc060b1d056f
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-07-22 19:17:54 -0400

    Update store_data.py

--- store_data.py
+++ store_data.py
@@ -1,2 +1,2 @@
 StoreItemsVar = {"Brew": 30, "Giant Brew": 50, "Brewcoin Doubler (2h)": 12, "Make Brewbot Open Source": 1500}
-hi = '''Brewbot is unfortunately currently closed source because it's BAD. '''
+hi = '''Brewbot is unfortunately currently closed source because of the rat TheRuntingMuumuu. Together we can stop this!!! Save up 1500 brewcoin for the perk!!!! Help me other people. YOu're my only hope.'''
