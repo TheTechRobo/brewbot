5b72e883c3d44db9be5707f0f4773f209e153d0e
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-07-22 18:08:07 -0400

    untested gtg 2 dinner

--- addBrewcoin.py
+++ addBrewcoin.py
@@ -1,36 +1,52 @@
 import json
 class NiceTry(Exception):
     pass
-def addbrewcoin(amount, user, usemultiplyer=True):
-    amount = float(amount)
 
-    with open("scores.json","r") as file:
-        scores = json.load(file)
-
-    try: #gets the multiplyer
-        multiplyer = float(scores["multiplyers"][user])
-    except KeyError: #if none, assigns 0
-        scores["multiplyers"][str(user)] = 1
-        multiplyer = 1
-    if not usemultiplyer:
-        multiplyer = 1
-
-    try: #assigns the brewcoins
-        userCoin = float(scores["scores"][user])
-        userCoin += (amount * multiplyer)
-        scores["scores"][user] = userCoin
-    except KeyError: #if user has none, assigns the amount
-        userCoin = multiplyer * amount
-        scores["scores"][user] = userCoin
-
-    with open('scores.json', 'w+') as confs:
-        confs.write(json.dumps(scores, indent=4))
-    return multiplyer*amount, userCoin, multiplyer
-
-def rembrewcoin(**kwargs):
-    amount = float(kwargs['amount'])
-    if amount < 0:
-        raise NiceTry("YOU GOT CAUGHT HAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA\nYou pay the person you stole from **1** brewcoin.")
-    amount = 0 - amount #https://stackoverflow.com/a/67205684/9654083, we know it's postive since, well... 1 line above
-    kwargs['amount'] = amount
-    addbrewcoin(**kwargs)
+class scores:
+    pass
+class json:
+    def __init__(self, filename="scores.json"):
+        self.filename = filename
+        self.scores = {}
+    def read(self):
+        with open(self.filename) as file:
+            self.scores = file.read()
+    def write(self):
+        with open(self.filename, "w+") as file:
+            file.write(json.dumps(self.scores, indent=4))
+    def addBrewcoin(amountount, user, usemultiplyer=True):
+        amount = float(amount)
+        try:
+            multiplyer = float(scores['multiplyers'][user])
+        except KeyError: #if their multiplyer doesn't exist they need to be reregistered
+            scores['multiplyers'][str(user)] = 1
+            multiplyer = 1
+        if not usemultiplyer:
+            multiplyer = 1
+        try:
+            userCoin = float(scores['scores'][user])
+            userCoin += (amount * multiplyer)
+            scores['scores'][user] = userCoin
+        except KeyError: #if user has none they need to be registered
+            userCoin = multiplyer * amount
+            scores['scores'][user] = userCoin
+        return multiplyer*amount, userCoin, multiplyer
+    def remBrewcoin(**kwargs):
+        amount = float(kwargs['amount'])
+        if amount < 0:
+            raise NiceTry("YOU GOT CAUGHT HAHHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHAHA\nYou pay the person you stole from **1** brewcoin.")
+        amount = 0 - amount ##https://stackoverflow.com/a/67205684/9654083, we know it's postive since, well... 1 line above
+        kwargs['amount'] = amount
+        return self.addBrewcoin(**kwargs)
+def addbrewcoin(*args, **kwargs):
+    obj = scores.json()
+    obj.read()
+    a = obj.addBrewcoin(*args, **kwargs)
+    obj.write()
+    return a
+def rembrewcoin(*args, **kwargs):
+    obj = scores.json()
+    obj.read()
+    a = obj.remBrewcoin(*args, **kwargs)
+    obj.write()
+    return a
