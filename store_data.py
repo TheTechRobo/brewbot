02ade2b44a6d19eea5635d38bd66c313c61830a7
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-07-23 21:46:39 -0400

    wip

--- store_data.py
+++ store_data.py
@@ -1,2 +1,6 @@
-StoreItemsVar = {"Brew": 30, "Giant Brew": 50, "Brewcoin Doubler (2h)": 12, "Make Brewbot Open Source": 1500}
+StoreItemsVar = {"Brew": 30, "Giant Brew": 50, "Brewcoin Doubler (2h)": 12, "Make Brewbot Open Source": 1500, "x2 multiplyer (4h)": 20}
 hi = '''Brewbot is unfortunately currently closed source because of the rat TheRuntingMuumuu. Together we can stop this!!! Save up 1500 brewcoin for the perk!!!! Help me other people. YOu're my only hope.'''
+
+def addX2Mult():
+    pass
+FunctionMap = {"x2 multiplyer (4h)": addX2Mult}
