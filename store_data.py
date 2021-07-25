a6fbf4d77d294eaa507c292c987d19881d1c2b38
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-07-24 21:32:10 -0400

    Small Changes

--- store_data.py
+++ store_data.py
@@ -1,6 +1,18 @@
+from addBrewcoin import scores
 StoreItemsVar = {"Brew": 30, "Giant Brew": 50, "Brewcoin Doubler (2h)": 12, "Make Brewbot Open Source": 1500, "x2 multiplyer (4h)": 20}
 hi = '''Brewbot is unfortunately currently closed source because of the rat TheRuntingMuumuu. Together we can stop this!!! Save up 1500 brewcoin for the perk!!!! Help me other people. YOu're my only hope.'''
 
-def addX2Mult():
-    pass
-FunctionMap = {"x2 multiplyer (4h)": addX2Mult}
+def addMult(user, val, time):
+    data = scores.json()
+    data.read()
+    data.checkMultiplyers(user)
+    try:
+        data.scores['multiplyers'][user]
+        if data.scores['multiplyers'][user] != 1:
+            raise KeyError
+    except KeyError:
+        return "ALREADY"
+    data.scores['multiplyers'][user] = val
+    data.scores['multiplyerTime'][user] =
+
+TypeMap = {"x2 multiplyer (4h)": "multiplyer"}
